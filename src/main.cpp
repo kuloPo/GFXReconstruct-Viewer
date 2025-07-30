4a3e6984bfec1de667d57f88cd71c6846b4c1300
kulopo <reg@kulopo.com>
2025-07-30 21:54:17 +0800

    Add GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <QApplication>
+#include <QWidget>
+
 #include <iostream>
 #include "format.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout<<"Hello GFXReconstruct Viewer!"<<std::endl;
-    return 0;
+
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    window.setWindowTitle("GFXReconstruct Viewer");
+
+    window.show();
+
+    return app.exec();
 }
